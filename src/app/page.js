805756a780ca4826aa5d5e6ee020f5846ce21a89
smckay1031 <smckay099@gmail.com>
2024-05-01 13:59:54 -0500

    added inter font to tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main>
-    <h1 className="text-5xl font-semibold"> Hello World</h1>
+    <h1 className="text-5xl font-semibold font-Inter"> Hello World</h1>
    </main>
   );
 }
