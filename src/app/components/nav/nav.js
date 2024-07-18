aaed45685c894386062b7b485140b33e35bc9fc9
smckay1031 <smckay099@gmail.com>
2024-07-18 16:29:49 -0500

    nav bar style updated

--- src/app/components/nav/nav.js
+++ src/app/components/nav/nav.js
@@ -4,13 +4,13 @@ import { NavButton} from "../auth/MultiFunctionButton";
 
 export  async function NavBar() {
     return (
-        <header className=" relative flex justify-center items-center font-Inter">
-            <div id='logo' className= "fixed top-0 py-2 px-5 w-full inline-flex justify-between items-center bg-slate-100 bg-opacity-5 border-b border-l border-slate-4 backdrop-blur-lg ">
+        <header className=" font-Inter flex items-center justify-center">
+            <div className= "fixed top-0 py-1 px-5 w-full inline-flex justify-between items-center bg-transparent">
                 <div className="flex gap-1 items-center">
-                <span className="font-semi-bold text-5xl font-Monoton">O</span>    
+                    <span className="font-semi-bold text-5xl font-Monoton">O</span>    
                 </div>
-               <nav> 
-                    <ul className="inline-flex gap-3 justify-center items-center cursor-pointer">
+               <nav className=""> 
+                    <ul className="inline-flex gap-4 justify-center items-center cursor-pointer">
                         <li className="hover:font-semibold">
                             <Link href={'/'}>Home</Link>
                         </li>
