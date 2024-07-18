d089c7b54d014aacf2832ad4442fad114f6dc59a
smckay1031 <smckay099@gmail.com>
2024-07-18 09:15:20 -0500

    edited style of login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,14 +1,15 @@
 //Login Page
-import { signIn } from "../../auth"
+
 import { Button } from "../components/auth/loginButton"
 
 
 
 export default async function Login() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-black">
-            <div className="bg-[#18D860] p-14 rounded-3xl bg-opacity-5 shadow-lg shadow-[#18d88537] border-[#18d86259] border h-96 max-w-xl flex justify-center items-center flex-col">
-                <h1 className="text-3xl my-10 font-bold font-Inter">Sign In To <a className="font-medium text-5xl font-Monoton">OctoGroove</a></h1>
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="bg-[#808080] p-14 rounded-3xl bg-opacity-5 shadow-lg shadow-[#18d8857e] border-[#ffffff27] border h-96 max-w-xl flex justify-center items-center flex-col">
+                <h1 className="font-medium text-5xl font-Monoton text-center my-10 opacity-95">OctaGroove</h1>
+                <h2 className="my-5 font-medium opacity-95 text-lg"> Sign In</h2>
                 <div className="flex-col items-center justify-center text-center">
                     <Button />
                 </div>
