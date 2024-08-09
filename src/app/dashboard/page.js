be46de6a6a9d838472763523f8a1f873a4e87bf4
smckay1031 <smckay099@gmail.com>
2024-08-08 21:33:07 -0500

    image styled top artists

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -16,17 +16,25 @@ export default async function Dashboard() {
     });
 
     const artists = await response.json()
-    console.log("Access_token:" + token);
     console.log(artists);
-
-   
-
 
 
     return(
         <main>
-        <h1 className=" mt-32">Dashboard</h1>
-        <p> {artists.items.name}</p>
+        <h1 className=" mt-32 p-10">Dashboard</h1>
+
+        <div className="font-Inter w-96">
+            <h2 className="font-Inter text-xl font-semibold"> Top Artists</h2>
+        <div className="grid grid-cols-4 gap-2 items-center" >
+            {artists.items.map((items) =>(
+                <div key={items.id} className="w-24 h-40">
+                    <img className="object-fill h-4/6" src={items.images[2].url} />
+                    <p className="text-sm font-light">{items.name}</p>
+                </div>
+            ))}
+        </div>
+       
+        </div>
     </main>
     )
 }
