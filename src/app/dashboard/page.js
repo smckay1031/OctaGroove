9162abd3b4e4d2f114407ef4db25334cb1c9847a
smckay1031 <smckay099@gmail.com>
2024-08-09 10:04:10 -0400

    styled artist with list style outside and no overlap of text and numbers

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -11,7 +11,7 @@ export default async function Dashboard() {
     const medTerm = 'medium_term';
     const longTerm = 'long_term';
 
-    const response = await fetch(`https://api.spotify.com/v1/me/top/artists?limit=8&offset=0&time_range=${medTerm}`, {
+    const response = await fetch(`https://api.spotify.com/v1/me/top/artists?limit=8&offset=0&time_range=${shortTerm}`, {
         headers: {
             'Authorization': 'Bearer ' + token
         }
@@ -21,13 +21,13 @@ export default async function Dashboard() {
 
     return(
         <main className="mt-16">
-        <div className="font-Inter w-max ml-5 bg-[#ffffff0a] px-4 pb-4 rounded-md shadow-md backdrop-blur border-[#ffffff10] border">
-            <h2 className="text-3xl font-bold text-center py-4"> Top Artists</h2>
-            <ul className="grid grid-cols-4 items-center w-full gap-2" >
+        <div className="font-Inter w-max ml-5 bg-[#ffffff0a] pl-6 pr-2 pb-4 rounded-md shadow-md backdrop-blur border-[#ffffff10] border">
+            <h2 className="text-3xl font-bold text-center py-2"> Top Artists</h2>
+            <ul className="grid grid-cols-4 items-center w-full" >
             {artists.items.map((items) =>(
-                <li key={items.id} className="list-decimal list-inside">
+                <li key={items.id} className="list-decimal list-outside font-bold text-sm mx-1">
                     <img className="object-cover h-24 w-24 rounded-md " src={items.images[2].url} />
-                    <p className="text-xs leading-tight">{items.name}</p>
+                    <p className="text-xs leading-tight font-light ml-1 w-24 h-8">{items.name}</p>
                  </li>
             ))}
             </ul>
