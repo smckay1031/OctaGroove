71b3f9f586c1aef9e2604ac62515aaf1478d1735
smckay1031 <smckay099@gmail.com>
2024-08-08 22:42:49 -0500

    layout styled with list for top artists

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,9 +7,11 @@ export default async function Dashboard() {
 
     const session = await auth();
     const token = session.user.access_token
-    
+    const shortTerm = 'short_term';
+    const mediumTerm = 'medium_term';
+    const longTerm = 'long_term';
 
-    const response = await fetch('https://api.spotify.com/v1/me/top/artists?limit=8&offset=0&time_range=long_term', {
+    const response = await fetch(`https://api.spotify.com/v1/me/top/artists?limit=8&offset=0&time_range=${longTerm}`, {
         headers: {
             'Authorization': 'Bearer ' + token
         }
@@ -23,17 +25,16 @@ export default async function Dashboard() {
         <main>
         <h1 className=" mt-32 p-10">Dashboard</h1>
 
-        <div className="font-Inter w-96">
-            <h2 className="font-Inter text-xl font-semibold"> Top Artists</h2>
-        <div className="grid grid-cols-4 gap-3 items-center" >
+        <div className="font-Inter w-5/12 ml-5">
+            <h2 className="font-Inter text-xl font-bold text-center py-5"> Top Artists</h2>
+            <ul className="grid grid-cols-4 items-center w-full" >
             {artists.items.map((items) =>(
-                <div key={items.id} className="w-24 h-40">
-                    <img className="object-cover h-4/6 rounded-md " src={items.images[2].url} />
-                    <p className="text-sm font-light">{items.name}</p>
-                </div>
+                <li key={items.id} className="list-decimal list-outside m-1">
+                    <img className="object-cover h-24 w-24 rounded-md " src={items.images[2].url} />
+                    <p className="text-xs leading-tight p-1">{items.name}</p>
+                 </li>
             ))}
-        </div>
-       
+            </ul>
         </div>
     </main>
     )
