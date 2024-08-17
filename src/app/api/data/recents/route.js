d4e0e1cfc655b4c8668e21862f516edbb6a7976e
smckay1031 <smckay099@gmail.com>
2024-08-17 16:14:26 -0400

    created api endpoint to fetch recentlyplayed songs

---
+++ src/app/api/data/recents/route.js
@@ -0,0 +1,14 @@
+import { auth } from "../../../../auth"
+
+export async function GET() {
+    const session = await auth()
+    const token = session.user.access_token;
+
+    const response = await fetch('https://api.spotify.com/v1/me/player/recently-played ', {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = await response.json();
+    return Response.json(data)
+}
