e7b0f3433d7941b7917f8091fe19b32c081e3e5c
smckay1031 <smckay099@gmail.com>
2024-08-17 16:23:21 -0400

    api revalidates data

--- src/app/api/data/recents/route.js
+++ src/app/api/data/recents/route.js
@@ -4,11 +4,11 @@ export async function GET() {
     const session = await auth()
     const token = session.user.access_token;
 
-    const response = await fetch('https://api.spotify.com/v1/me/player/recently-played ', {
+    const response = await fetch('https://api.spotify.com/v1/me/player/recently-played ',{
         headers: {
             'Authorization': 'Bearer ' + token
         }
-    })
+    }, {next: { revalidate: 1800 } })
     const data = await response.json();
     return Response.json(data)
 }
