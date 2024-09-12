a98c7a75077dfae44299563ffae1f32ce0c0438e
smckay1031 <smckay099@gmail.com>
2024-09-12 09:50:04 -0400

    fixed padding of explore card

--- src/app/components/dashboard/explore.js
+++ src/app/components/dashboard/explore.js
@@ -26,7 +26,7 @@ function Recents(){
         }
 
         return (
-            <div id="recents" className="overflow-y-scroll h-80 py-2 rounded-2xl">
+            <div id="recents" className="overflow-y-scroll h-80 p-2 rounded-2xl">
                 <ul className="capitalize leading-none flex flex-wrap justify-center items-center gap-1">
                     {data.items.map((item, index)=>(
                         <li key={index} className=" text-xs mt-1">
