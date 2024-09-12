6ea4767e95dc2bf7aebedf98c90d973e69641381
smckay1031 <smckay099@gmail.com>
2024-09-12 18:54:15 -0400

    padding changes to explore card

--- src/app/components/dashboard/explore.js
+++ src/app/components/dashboard/explore.js
@@ -1,5 +1,5 @@
 'use client'
-import { useState} from "react"
+import { useState } from "react"
 import useSWR from "swr";
 import fetcher from "../../../../lib/fetcher";
 import Loading from "../UI/loading"
@@ -11,7 +11,7 @@ function Recents(){
         focusThrottleInterval: 120000,
         revalidateIfStale:false,
         revalidateOnFocus: false,
-        refreshInterval: 120000,
+        refreshInterval: 100000,
     })
     if(isLoading || isValidating ){
         return (
@@ -33,8 +33,8 @@ function Recents(){
                             <div className="h-24 w-24 overflow-hidden relative hover:scale-110 duration-500"> 
                                 <img src={item.track.album.images[1].url} className="h-24 w-24 rounded-2xl border border-[#00000040]" /> 
                                 <div className="absolute top-0 scale-50 opacity-0 left-0 rounded-2xl w-24 h-24 bg-[#00000040] backdrop-blur-sm border border-[#ffffff35] flex flex-col justify-center items-center translate-y-0 duration-500 p-0.5 cursor-pointer z-50 hover:opacity-100 hover:scale-100">
-                                    <a href={item.track.uri} target="_blank" className=" font-bold leading-none hover:underline mb-0.5 max-h-12 overflow-hidden">{item.track.name}</a>
-                                    <a className="text-xs font-extralight leading-none">{Artists(item)}</a>
+                                    <a href={item.track.uri} target="_blank" className=" font-bold leading-none hover:underline pb-0.5 max-h-12 overflow-hidden">{item.track.name}</a>
+                                    <a className="text-xs font-extralight leading-none overflow-hidden pb-0.5">{Artists(item)}</a>
                                 </div>
                             </div>
                         </li>
@@ -50,7 +50,7 @@ function Reccomend() {
     const {data, isLoading, isValidating} = useSWR('/api/data/reccomend', fetcher, {
         revalidateIfStale: false,
         revalidateOnFocus: false,
-        focusThrottleInterval: 900000,
+        focusThrottleInterval: 1000000,
     })
     if(isLoading || isValidating) {
         return(<div className="h-80 flex justify-center items-center font-Inter text-2xl"><p className=" animate-pulse duration-500 font-semibold">Loading </p> <Loading /> </div>)
@@ -72,8 +72,8 @@ function Reccomend() {
                             <div className="h-24 w-24 overflow-hidden relative hover:scale-110 duration-500"> 
                                 <img src={item.album.images[1].url} className="h-24 w-24 rounded-2xl border border-[#00000040]" /> 
                                 <div className="absolute top-0 scale-50 opacity-0 left-0 rounded-2xl w-24 h-24 bg-[#00000040] backdrop-blur-sm border border-[#ffffff35] flex flex-col justify-center items-center translate-y-0 duration-500 p-0.5 cursor-pointer z-50 hover:opacity-100 hover:scale-100">
-                                    <a href={item.uri} target="_blank" className=" font-bold leading-none hover:underline mb-0.5">{item.name}</a>
-                                    <a className="text-xs font-extralight leading-none">{Artists(item)}</a>
+                                    <a href={item.uri} target="_blank" className=" font-bold leading-none hover:underline pb-0.5 max-h-12 overflow-hidden">{item.name}</a>
+                                    <a className="text-xs font-extralight leading-none overflow-hidden pb-0.5">{Artists(item)}</a>
                                 </div>
                             </div>
                         </li>
@@ -89,7 +89,6 @@ function Reccomend() {
 export default function ExploreCard() {
 
     const [toggle, setToggle] = useState('recents');
-
     const recentActive = toggle === 'recents'
     const reccomendAvtive = toggle == 'reccomend'
     
