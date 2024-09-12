ce7a48948f9e9ad98eaf662041985caad3b92482
smckay1031 <smckay099@gmail.com>
2024-09-12 18:05:01 -0400

    adjusted top margin or reccomend to match layout of recents

--- src/app/components/dashboard/explore.js
+++ src/app/components/dashboard/explore.js
@@ -68,7 +68,7 @@ function Reccomend() {
             <div id="recents" className=" overflow-y-scroll h-80 py-2 px-1">
                 <ul className="capitalize leading-none flex flex-wrap justify-center items-center gap-1">
                     {data.tracks.map((item)=>(
-                        <li key={item.id} className=" text-xs">
+                        <li key={item.id} className=" text-xs mt-1">
                             <div className="h-24 w-24 overflow-hidden relative hover:scale-110 duration-500"> 
                                 <img src={item.album.images[1].url} className="h-24 w-24 rounded-2xl border border-[#00000040]" /> 
                                 <div className="absolute top-0 scale-50 opacity-0 left-0 rounded-2xl w-24 h-24 bg-[#00000040] backdrop-blur-sm border border-[#ffffff35] flex flex-col justify-center items-center translate-y-0 duration-500 p-0.5 cursor-pointer z-50 hover:opacity-100 hover:scale-100">
