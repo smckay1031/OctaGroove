d3b1ef8b28e1e78a13b45ab75f17ec1c73bb1247
smckay1031 <smckay099@gmail.com>
2024-09-15 21:43:24 -0400

    side bar font-weight and bullets added

--- src/app/components/dashboard/sidebar.jsx
+++ src/app/components/dashboard/sidebar.jsx
@@ -63,9 +63,9 @@ export default async function SideBar() {
                 <div className="p-1">
                     <ul className=" overflow-y-scroll">
                         {playlists.items.map((list) =>(
-                            <div key={list.id} className="text-sm leading-none py-0.5 pl-5 my-0.5">
-                                <li className="capitalize">
-                                    <a href={list.uri} target="_blank" className=" no-underline font-extralight hover:font-semibold hover:scale-110 hover:text-[#18D860] duration-300">- {list.name}</a>
+                            <div key={list.id} className="text-sm leading-none py-0.5 pl-8 my-0.5">
+                                <li className="capitalize list-disc">
+                                    <a href={list.uri} target="_blank" className="no-underline font-normal hover:font-semibold hover:scale-110 hover:text-[#18D860] duration-300">{list.name}</a>
                                 </li>
                             </div>
                         ))}
