5d4379da9a3654c9652e148c5b99fa9237efee61
smckay1031 <smckay099@gmail.com>
2024-09-24 21:48:54 -0400

    padding and gap added to flex and footer divs

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -6,14 +6,14 @@ export default function Footer() {
 
     return(
         <footer className="max-w-full font-Inter mt-10 bg-black rounded-lg">
-            <div className="flex justify-between items-end p-5">
-                <p className="pb-1 lg:text-base text-sm"> Created By Sean McKay</p>
-                <div className="flex flex-col items-center justify-center pb-4">
+            <div className="flex justify-between items-end p-5 gap-1">
+                <p className="pb-1 lg:text-base text-xs"> Created By Sean McKay</p>
+                <div className="flex flex-col items-center justify-center pb-4 px-1">
                     <p className="lg:text-lg text-sm font-semibold py-4">Navigation</p>
                         <Link href={'/'} className="lg:text-sm text-xs hover:font-bold">Home</Link>
                         <Link href={'/dashboard/range_short'} className="lg:text-sm text-xs hover:font-bold">Dashboard</Link>
                 </div>
-                <div className="flex flex-col items-center justify-center pb-4">
+                <div className="flex flex-col items-center justify-center pb-4 px-1">
                     <p className="md:text-lg text-sm font-semibold py-4">Legal</p>
                         <Link href={'/terms'} className="lg:text-sm text-xs hover:font-bold"> Terms and Conditions</Link>
                         <Link href={'/privacy'} className="lg:text-sm text-xs hover:font-bold"> Privacy Policy</Link>
