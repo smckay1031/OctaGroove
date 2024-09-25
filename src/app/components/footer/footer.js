f42c8dbc633565b9136cd646f8d1d19610d56bac
smckay1031 <smckay099@gmail.com>
2024-09-24 22:33:33 -0400

    footer responsive done

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -6,25 +6,19 @@ export default function Footer() {
 
     return(
         <footer className="max-w-full font-Inter mt-10 bg-black rounded-lg">
-            <div className="flex justify-between items-end p-5 gap-1">
-                <p className="pb-1 lg:text-base text-xs"> Created By Sean McKay</p>
-                <div className="flex flex-col items-center justify-center pb-4 px-1">
-                    <p className="lg:text-lg text-sm font-semibold py-4">Navigation</p>
-                        <Link href={'/'} className="lg:text-sm text-xs hover:font-bold">Home</Link>
-                        <Link href={'/dashboard/range_short'} className="lg:text-sm text-xs hover:font-bold">Dashboard</Link>
-                </div>
-                <div className="flex flex-col items-center justify-center pb-4 px-1">
-                    <p className="md:text-lg text-sm font-semibold py-4">Legal</p>
-                        <Link href={'/terms'} className="lg:text-sm text-xs hover:font-bold"> Terms and Conditions</Link>
-                        <Link href={'/privacy'} className="lg:text-sm text-xs hover:font-bold"> Privacy Policy</Link>
-                </div>
+            <div className="flex justify-between items-end p-5">
+                <p className="pb-1 lg:text-sm text-xs"> Created By Sean McKay</p>
+                    <div className="flex justify-center gap-4">
+                        <Link href={'/terms'} className=" lg:text-sm text-xs font-thin lg:font-normal hover:font-bold pb-1 text-center"> Terms and Conditions</Link>
+                        <Link href={'/privacy'} className="lg:text-sm text-xs font-thin lg:font-normal hover:font-bold text-center"> Privacy Policy</Link>
+                    </div>
                 <div className="flex flex-col items-center justify-center pb-2">
                     <a href="https://github.com/smckay1031/OctaGroove" target="_blank">
                     <Image
                     src={Github}
                     alt="Github"
-                    height={36}
-                    width={36} />
+                    height={28}
+                    width={28} />
                     </a>
                 </div>
             </div>
