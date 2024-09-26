0aa2330b3e0b1853d77a4ed0ade506b8eb09c10b
smckay1031 <smckay099@gmail.com>
2024-09-26 10:33:26 -0400

    edited text of about on home

--- src/app/page.js
+++ src/app/page.js
@@ -47,7 +47,7 @@ export default function Home() {
       <article className="flex flex-col items-center justify-center py-64 lg:px-64 px-10 " >
         <h3 className="lg:text-6xl text-5xl pb-8 font-bold px-8">About</h3>
         <div className="flex flex-wrap items-center justify-center w-full">
-          <p className=" w-1/2 flex-grow pt-20 px-5"> <span className="font-bold text-lg">OctaGroove</span> is an online dashboard designed to be used with your Spotify app to enhance your listening experience. You will have an <span className="font-bold text-lg">enhanced UI experience</span> that allows you to view your top eight items. <span className="font-bold text-lg">Your data is never stored on our servers and is never shared with anyone.</span> Simply sign in to your Spotify account to get started.<br /> <span className="font-bold text-lg"> Please insure cookies are accepted for this app to work properly.</span></p>
+          <p className=" w-1/2 flex-grow pt-20 px-5"><span className="font-bold text-lg">Welcome to OctaGroove</span>!<br /> OctaGroove lets you see your top 8 artists, tracks, genres, and more from Spotify.<br />  <span className="font-bold text-lg">OctaGroove</span> is an online dashboard designed to be used with your Spotify app to enhance your listening experience. You will have an <span className="font-bold text-lg">enhanced UI experience</span> that allows you to view your top eight items. <span className="font-bold text-lg">Your data is never stored on our servers and is never shared with anyone.</span> Simply sign in to your Spotify account to get started.<br /> <span className="font-bold text-lg"> Please insure cookies are accepted for this app to work properly.</span></p>
           <Image
           src={ScreenShot}
           alt="dashboard"
